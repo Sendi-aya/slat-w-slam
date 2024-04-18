e5bc21397ab4eeeb83e17ea29f9c9046f8e2ff75
Sendi-aya <ayasendidd@gmail.com>
2024-04-18 19:00:17 +0200

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+         /********************définir la taille String <=> varchar*****************************/
+
+
+         Schema::defaultStringLength(200);
+
+    }
+}
